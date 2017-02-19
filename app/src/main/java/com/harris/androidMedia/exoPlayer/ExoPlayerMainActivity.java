2517db7b5a48a5132377d77e976a39848325b7a9
Harris <mjw090608@gmail.com>
2017-02-19 16:04:07 +0800

    modify ui

--- app/src/main/java/com/harris/androidMedia/exoPlayer/ExoPlayerMainActivity.java
+++ app/src/main/java/com/harris/androidMedia/exoPlayer/ExoPlayerMainActivity.java
@@ -14,7 +14,7 @@ import com.harris.androidMedia.databinding.ActivityExoplayerBinding;
  * Created by Harris on 2017/2/18.
  */
 
-public class ExoPlayerEntrance extends AppCompatActivity implements View.OnClickListener {
+public class ExoPlayerMainActivity extends AppCompatActivity implements View.OnClickListener {
 
     ActivityExoplayerBinding binding;
     private String uri;
@@ -22,25 +22,38 @@ public class ExoPlayerEntrance extends AppCompatActivity implements View.OnClick
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         binding = DataBindingUtil.setContentView(this, R.layout.activity_exoplayer);
         setSupportActionBar(binding.toolbar);
         uri = "http://odzl05jxx.bkt.clouddn.com/%E6%9D%8E%E5%BF%97&quot;%E7%9C%8B%E8%A7%81&quot;2015%E5%B7%A1%E6%BC%94%E9%A2%84%E5%91%8A%E7%89%87.mp4";
-        binding.fab.setOnClickListener(this);
+        binding.card1.setOnClickListener(this);
+        binding.card2.setOnClickListener(this);
+        binding.card3.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
+        Intent intent = null;
         switch (v.getId()) {
-            case R.id.fab:
-                Intent intent = new Intent(this, SimpleExoPlayerViewActivity.class);
+            case R.id.card1:
+                intent = new Intent(this, SimpleExoPlayerViewActivity.class);
                 String[] urlArray = new String[]{uri};
                 intent.putExtra(SimpleExoPlayerViewActivity.URI_LIST_EXTRA, urlArray);
                 intent.setAction(SimpleExoPlayerViewActivity.ACTION_VIEW_LIST);
-                startActivity(intent);
+                break;
+            case R.id.card2:
+                intent = new Intent(this, SurfaceViewPlayerActivity.class);
+               /* String[] urlArray = new String[]{uri};
+                intent.putExtra(SimpleExoPlayerViewActivity.URI_LIST_EXTRA, urlArray);
+                intent.setAction(SimpleExoPlayerViewActivity.ACTION_VIEW_LIST);*/
+                break;
+            case R.id.card3:
+                intent = new Intent(this, TextureViewActivity.class);
                 break;
             default:
                 break;
+        }
+        if (intent != null) {
+            startActivity(intent);
         }
     }
 }
