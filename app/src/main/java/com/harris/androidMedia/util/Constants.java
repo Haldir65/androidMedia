c08ae6766eb2804ac2af95c506c67aec17452fd4
Harris <mjw090608@gmail.com>
2017-05-14 19:20:11 +0800

    no more bill

--- app/src/main/java/com/harris/androidMedia/util/Constants.java
+++ app/src/main/java/com/harris/androidMedia/util/Constants.java
@@ -7,16 +7,16 @@ package com.harris.androidMedia.util;
 public class Constants {
 
 
-    public static final String Mp4uri = "http://odzl05jxx.bkt.clouddn.com/%E6%9D%8E%E5%BF%97&quot;%E7%9C%8B%E8%A7%81&quot;2015%E5%B7%A1%E6%BC%94%E9%A2%84%E5%91%8A%E7%89%87.mp4";
-    public static final String Mp4Url3 = "http://odzl05jxx.bkt.clouddn.com/%E6%9D%8E%E5%BF%97%20%E8%B7%A8%E5%B9%B42013%20%E5%8B%BE%E4%B8%89%E6%90%AD%E5%9B%9B%20%E8%A2%AB%E7%A6%81%E5%BF%8C%E7%9A%84%E6%B8%B8%E6%88%8F_%E9%AB%98%E6%B8%85.mp4";
-    public static final String Mp4Url2 = "http://odzl05jxx.bkt.clouddn.com/%E9%99%88%E7%B2%92-%E8%A5%BF%E6%B9%96%E9%9F%B3%E4%B9%90%E8%8A%82.MP4";
+    public static final String Mp4uri = "/ola.mp4";
+    public static final String Mp4Url3 = "/crazy_2014.mp4";
+    public static final String Mp4Url2 = "";
 
-    public static final String RMVBuri = "http://odzl05jxx.bkt.clouddn.com/girl.rmvb";
-    public static final String flvString = "http://odzl05jxx.bkt.clouddn.com/CINDERELLA%20--%20Moccona%20%28quest%29_%E6%A0%87%E6%B8%85.flv";
+    public static final String RMVBuri = "/girl.rmvb";
+    public static final String flvString = "";
 
-    public static final String JPEGUrl = "http://odzl05jxx.bkt.clouddn.com/DSCN2948.JPG";
+    public static final String JPEGUrl = "";
 
-    public static final String mp3Url = "http://odzl05jxx.bkt.clouddn.com/%E5%8E%9F%E5%A3%B0%E5%B8%A6%20-%20Cloud%20Atlas%20Finale.mp3";
+    public static final String mp3Url = "";
 
     public static final String PATH_CAMERA2 = "camera2Basic";
 
@@ -26,10 +26,10 @@ public class Constants {
 
     public static final String FILE_FORMAT_PNG = ".png";
 
-    public static final String IMG_URL_1 = "http://odzl05jxx.bkt.clouddn.com/minion.jpg";
-    public static final String IMG_URL_2 = "http://odzl05jxx.bkt.clouddn.com/rationalizingyourhoriiblehack-big.png";
-    public static final String IMG_URL_3 = "http://odzl05jxx.bkt.clouddn.com/implementingdumbfeatures-big.png";
-    public static final String IMG_URL_4 = "http://odzl05jxx.bkt.clouddn.com/writing%20code%20that%20nobody%20else%20can%20read.jpg";
-    public static final String IMG_URL_5 = "http://odzl05jxx.bkt.clouddn.com/Trying%20stuff%20Untill%20it%20work.jpg";
+    public static final String IMG_URL_1 = "http://";
+    public static final String IMG_URL_2 = "http://";
+    public static final String IMG_URL_3 = "http://";
+    public static final String IMG_URL_4 = "http://";
+    public static final String IMG_URL_5 = "http://";
 
 }
