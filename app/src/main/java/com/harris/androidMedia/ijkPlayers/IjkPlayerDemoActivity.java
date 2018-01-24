f989cfae0d4a62cf169976ba39d188ca6d8e4dc7
Haldir65 <mjw090608@gmail.com>
2018-01-24 11:35:51 +0800

    integrate ijkplayer

--- app/src/main/java/com/harris/androidMedia/ijkPlayers/IjkPlayerDemoActivity.java
+++ app/src/main/java/com/harris/androidMedia/ijkPlayers/IjkPlayerDemoActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import com.harris.androidMedia.R;
+
 /**
  * Created by Fermi on 2017/8/16.
  */
@@ -14,6 +16,8 @@ public class IjkPlayerDemoActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        setContentView();
+        setContentView(R.layout.activity_ijk_player);
 
     }
 
