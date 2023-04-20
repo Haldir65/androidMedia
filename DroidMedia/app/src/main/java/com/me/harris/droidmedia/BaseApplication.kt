ce1cabd6a4ae5fcbafd0f98ae222eb218ea1954c
Haldir65 <mjw090608@gmail.com>
2023-04-20 19:15:35 +0800

    updates

--- DroidMedia/app/src/main/java/com/me/harris/droidmedia/BaseApplication.kt
+++ DroidMedia/app/src/main/java/com/me/harris/droidmedia/BaseApplication.kt
@@ -1,7 +1,9 @@
 package com.me.harris.droidmedia
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class BaseApplication:Application() {
 
     companion object {
