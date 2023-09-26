bfb16f10fc688d48dae598524221fc17b7375339
Haldir65 <mjw090608@gmail.com>
2023-09-26 15:15:30 +0800

    cp code of androidVideoCache for analysis

---
+++ DroidMedia/medialibs/videocache/build.gradle.kts
@@ -0,0 +1,56 @@
+@Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+}
+val COMPILE_SKD_VERSION:String by project
+val MIN_SDK_VERSION:String by project
+
+android {
+    namespace = "com.danikula.videocache"
+    compileSdk = COMPILE_SKD_VERSION.toInt()
+    buildToolsVersion = "34.0.0"
+
+    buildFeatures {
+        viewBinding = true
+        buildConfig = true
+    }
+
+    defaultConfig {
+        minSdk = libs.versions.minSdkVersion.get().toInt()
+        targetSdk =  libs.versions.targetSdkVersion.get().toInt()
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.core.ktx)
+    implementation(libs.androidx.appcompat.appcompat)
+    implementation(libs.google.android.material.material)
+    testImplementation(libs.junit.junit)
+    androidTestImplementation(libs.androidx.test.junit)
+    androidTestImplementation(libs.androidx.test.espresso.espresso.core)
+
+    implementation(libs.slfj.android)
+
+}
