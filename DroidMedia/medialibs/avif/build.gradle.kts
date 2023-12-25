9afe6148a349785b46bfaec63d6b818ee1260e65
Haldir65 <mjw090608@gmail.com>
2023-12-25 22:22:55 +0800

    [gh action] copy oboe header

--- DroidMedia/medialibs/avif/build.gradle.kts
+++ DroidMedia/medialibs/avif/build.gradle.kts
@@ -2,7 +2,7 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
-    id("com.google.devtools.ksp").version("1.9.10-1.0.13") // Or latest version of KSP
+    id("com.google.devtools.ksp").version("1.9.21-1.0.16") // Or latest version of KSP
 }
 
 val SUPPORT_NATIVE_BUILD:String  by project
