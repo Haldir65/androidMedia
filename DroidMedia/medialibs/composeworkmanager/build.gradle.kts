c1c99d04df838e921c5fa6b24de250ee826d2bce
Haldir65 <mjw090608@gmail.com>
2024-05-05 22:21:17 +0800

    [Build] [Docker] update kotlin_verison to 2.0.0-rc2

--- DroidMedia/medialibs/composeworkmanager/build.gradle.kts
+++ DroidMedia/medialibs/composeworkmanager/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    alias(libs.plugins.compose.compiler)
+
 }
 
 android {
@@ -33,9 +35,11 @@ android {
     buildFeatures {
         compose = true
     }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.11"
-    }
+
+
+//    composeOptions {
+//        kotlinCompilerExtensionVersion = "1.5.11"
+//    }
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
