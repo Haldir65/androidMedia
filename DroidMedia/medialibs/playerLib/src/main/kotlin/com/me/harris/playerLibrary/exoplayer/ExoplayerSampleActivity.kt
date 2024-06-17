6fb118ef2dc25fe7c8bf997e16f7dbdbac368e0c
Haldir65 <mjw090608@gmail.com>
2024-06-17 14:04:52 +0800

    [feat] modify prebuild container info

--- DroidMedia/medialibs/playerLib/src/main/kotlin/com/me/harris/playerLibrary/exoplayer/ExoplayerSampleActivity.kt
+++ DroidMedia/medialibs/playerLib/src/main/kotlin/com/me/harris/playerLibrary/exoplayer/ExoplayerSampleActivity.kt
@@ -3,14 +3,22 @@ package com.me.harris.playerLibrary.exoplayer
 import android.annotation.SuppressLint
 import android.os.Build
 import android.os.Bundle
+import android.os.Handler
+import android.util.Log
 import android.view.View
 import androidx.annotation.OptIn
 import androidx.appcompat.app.AppCompatActivity
 import androidx.media3.common.MediaItem
 import androidx.media3.common.util.UnstableApi
-import androidx.media3.common.util.Util
 import androidx.media3.exoplayer.DefaultLoadControl
+import androidx.media3.exoplayer.DefaultRenderersFactory
 import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.exoplayer.Renderer
+import androidx.media3.exoplayer.audio.AudioRendererEventListener
+import androidx.media3.exoplayer.metadata.MetadataOutput
+import androidx.media3.exoplayer.text.TextOutput
+import androidx.media3.exoplayer.video.DecoderVideoRenderer
+import androidx.media3.exoplayer.video.VideoRendererEventListener
 import com.me.harris.awesomelib.utils.VideoUtil
 import com.me.harris.awesomelib.viewBinding
 import com.me.harris.playerLibrary.R
@@ -41,6 +49,33 @@ class ExoplayerSampleActivity:AppCompatActivity(R.layout.activity_exoplayer_samp
             DefaultLoadControl.DEFAULT_BUFFER_FOR_PLAYBACK_AFTER_REBUFFER_MS)
             .build()
         player = ExoPlayer.Builder(this).setLoadControl(loadControl)
+            .setRenderersFactory(object :DefaultRenderersFactory(this){
+               init {
+                   setExtensionRendererMode(EXTENSION_RENDERER_MODE_PREFER)
+               }
+
+                override fun createRenderers(
+                    eventHandler: Handler,
+                    videoRendererEventListener: VideoRendererEventListener,
+                    audioRendererEventListener: AudioRendererEventListener,
+                    textRendererOutput: TextOutput,
+                    metadataRendererOutput: MetadataOutput
+                ): Array<Renderer> {
+                    val red = super.createRenderers(
+                        eventHandler,
+                        videoRendererEventListener,
+                        audioRendererEventListener,
+                        textRendererOutput,
+                        metadataRendererOutput
+                    )
+                    red.filterIsInstance<DecoderVideoRenderer>().forEach { videoRender ->
+                        Log.e("videoRender" , "${videoRender.name}")
+
+                    }
+                    return red
+                }
+
+            })
             .build()
             .also { exoPlayer ->
                 binding.videoView.player = exoPlayer
